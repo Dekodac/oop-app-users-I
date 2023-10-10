aad436095fabfc9d8ddc20a98058e3d210bb68fe
Dakota Cannon <Dekodac@gmail.com>
2023-10-10 19:59:42 -0400

    completed

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
-# your User class goes here
+class User():
+    def __init__(self, name, dl, email):
+        self.name = name
+        self.email = email
+        self.dl = dl
+
+    def __str__(self):
+        return f"Name: {self.name}, DL: {self.dl}, Email: {self.email}"
+
+person1 = User("Matt", "23rhai3HD", "MattBleater394@aol.com")
+person2 = User("Pete", "2grrga3HD", "Peteskete@hotmail.com")
+person3 = User("S1mple", "2s4rfw23f", "S1mpleCsgo@microsoft.com")
+
+print(person1)
+print(person2)
+print(person3)
